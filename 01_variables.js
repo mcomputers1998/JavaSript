4815f3ee11aeb0a0ec17c5eeef3c1a97a6cad1de
mcomputers1998 <mcomputers.ich@gmail.com>
2024-01-20 11:50:48 +0530

    Initial Commit From Vscode

---
+++ 01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 1008
+let accountEmail="mcomputers.co.in"
+var accountPassword ="12345"
+accountCity = "Kolhapur"
+let accountState;
+//accountId = 2
+/* Prefer not to use var
+because of issue in block 
+*/
+
+accountEmail = "info@mcomputers.co.in"
+accountPassword="333"
+accountCity="Ichalkaranji"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";  // treat ll JS code as newer version
+// alert("Hello")   // we are using nodjs, now browser
+
+let name="Pankaj"
+let age = 18
+let isLoggedIn=false
+
+// --- data types  
+// int
+// bigint
+// string 
+// boolean = true / false
+// null    --> statndalone value
+// undefined -->
+// symbol --> unique 
+
+// object
+
+console.log(typeof(name))
+console.log(typeof(age))
+console.log(typeof(null))
+console.log(typeof(undefined))
+
+
